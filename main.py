99863a0525e070f58fda24d6e9ab0b54e1d73ece
j0lyn3 <evergloam1212@outlook.com>
2025-11-19 15:48:28 -0500

    Set up I/O and Executor URL fetch and process control along w/ corresponding args in the CLI - Jolye Ross

--- main.py
+++ main.py
@@ -14,18 +14,20 @@ def build_parser():
         title="Programs"
     )
 
+    ## base data manip args
     p_csv_manip = subparsers.add_parser("data-manip", help="Manipulate a Media Cloud CSV file")
     p_csv_manip.add_argument("-i", "--input", required=True, help="Media Cloud dataset file for input", type=pathlib.Path)
     p_csv_manip.add_argument("-q", "--pd_query", help="The pandas style query to draw from (Default: None)")
     p_csv_manip.add_argument("--columns", nargs="+", help="Optional list of columns to keep in the output")
     p_csv_manip.add_argument("-o", "--output", type=pathlib.Path, required=True, help="Output CSV file for the sub-sample")
-    #p_csv_manip.add_argument("--sub-sample", nargs=2, metavar=("pd_query", ""), help="sub-sample the input dataset w/ a pandas query")
+
     csv_subparsers = p_csv_manip.add_subparsers(
         dest="function",
         required=True,
         title="Functions"
     )
 
+    ## sub sampler args
     csv_sub_sample = csv_subparsers.add_parser("sub-sample", help="Sub Sample the CSV File (using pandas sampling)")
 
     size_group = csv_sub_sample.add_mutually_exclusive_group(required=True)
@@ -37,10 +39,16 @@ def build_parser():
 
     csv_sub_sample.set_defaults(func=csvManip.main, callback=csvManip.sub_sample)
 
+    ## data cleaner args
     csv_clean_data = csv_subparsers.add_parser("clean-data", help="Removes Duplicate articles and cleans up the data")
 
+
+    ## getter args
     p_get_articles = subparsers.add_parser("get-articles", help="Downloads the Articles from the given URL")
-    p_get_articles.add_argument("--csv", required=True, help="Media Cloud CSV File for input")
+    p_get_articles.add_argument("-i", "--input", required=True, help="Media Cloud CSV File for input")
+    p_get_articles.add_argument("-o", "--output", required=True, help="Where to write output (does not maintain order)")
+    p_get_articles.add_argument("--con-gets", default=50, help="# of concurrent HTTP Get Requests from given URLs")
+    p_get_articles.add_argument("--cores", default=4, help="# of cores to use for analysis step")
     p_get_articles.set_defaults(func=getArticles.main)
 
     return parser
@@ -51,7 +59,8 @@ def main():
 
     try: args.func(args, args.callback)
     except Exception as e:
-        print(e.with_traceback())
+        print(e)
         parser.print_help()
 
-main()
+if __name__ == "__main__":
+    main()
--- programs/getArticles.py
+++ programs/getArticles.py
@@ -1,2 +1,52 @@
-def main(args):
-    print(args)
+import asyncio
+import aiohttp
+from concurrent.futures import ProcessPoolExecutor
+from pandas import read_csv
+
+## cpu bound synchronous processing.
+def process_html(html: str):
+    print(html)
+
+## simple html fetch
+async def fetch_html(url: str, session: aiohttp.ClientSession):
+    try:
+        async with session.get(url, timeout=10.0) as resp:
+            resp.raise_for_status()
+            return await resp.text()
+    except Exception as e:
+        print(e)
+
+## i/o & cpu controller function
+async def fetch_process_write(url, session: aiohttp.ClientSession, pool: ProcessPoolExecutor, outFile):
+    ## fetches our raw html from the url using our session
+    html = await fetch_html(url=url, session=session)
+    if html == None: return None
+
+    ## grabs our loop set up back in main()
+    loop = asyncio.get_running_loop()
+    ## runs our synchronous function w/ our pool
+    future = loop.run_in_executor(pool, process_html, html)
+    ## await the results
+    result = await future
+
+    ## todo: write to file (filetype??)
+
+async def main(args, callback=None):
+    ## grab data using pandas
+    data = read_csv(args.input)[["url", "media_url"]]
+    urls = data["url"]
+
+    ## create our worker pool (this will manage out cpu bound processing!!)
+    with ProcessPoolExecutor(max_workers=args.cores) as pool:
+        ## create out http session which will manage our i/o bound getting
+        async with aiohttp.ClientSession() as session:
+            sem = asyncio.Semaphore(args.con_gets) ## num of concurrent url gets
+
+            ## our semaphore controlled async function
+            async def semmedAsync(url): 
+                ## pawns the semaphore controlled async to our session-pool controller
+                async with sem: return await fetch_process_write(url, session, pool, args.output)
+            
+            ## making running loop (this is what asyncio returns with get_running_loop())
+            tasks = [semmedAsync(url) for url in urls]
+            futures = await asyncio.gather(*tasks)
