5cb7fe1f17621f54c36eba9bd2ec85540f1dc135
j0lyn3 <evergloam1212@outlook.com>
2025-12-02 14:20:17 -0500

    Removed trans specific filters
    TODO: Read from file

--- programs/csvManip.py
+++ programs/csvManip.py
@@ -10,27 +10,7 @@ def sub_sample(args, data: pd.DataFrame) -> pd.DataFrame:
     )
 
 # A generous keyword list for trans-issue detection
-KEYWORDS = [
-    r"\btransgender\b", r"\btrans\b", r"\btrans woman\b", r"\btrans man\b",
-    r"\btranswomen\b", r"\btransmen\b", r"\btrans people\b",
-    r"\bnon[- ]?binary\b", r"\bnonbinary\b", r"\bgender[- ]?fluid\b",
-    r"\bgender[- ]?queer\b", r"\bagender\b", r"\bbigender\b",
-    r"\btransition\b", r"\btransitioning\b", r"\bdetransition\b", r"\bde-transition\b",
-    r"\bdysphoria\b",
-    r"\blgb[a-z0-9]*\b",
-    r"\bintersex\b",
-    r"\bpronoun\b", r"\bpronouns\b",
-    r"\bmisgender\b", r"\bmisgendered\b", r"\bmisgendering\b",
-    r"\bdeadname\b", r"\bdeadnamed\b",
-    r"\btransphobia\b", r"\btransphobic\b",
-    r"\bmtf\b", r"\bftm\b",
-    r"\bhormone\b", r"\bhormone therapy\b", r"\bhormone replacement\b",
-    r"\bpuberty blocker\b", r"\bpuberty blockers\b", r"\bpuberty\b"
-    r"\bcisgender\b", r"\bcis\b",
-    r"\bbiology\b", r"\bsports\b", r"\bbiological\b", r"\bbirth\b",
-    r"\btransgenderism\b", r"\bideology\b",
-    r"\brobinson\b", r"\bboyfriend\b", r"\bsuspect\b", r"\bassassin\b" ## news about the charlie kirk assassin is heavily represented; this limits the stories about it to the main thing concering trans issues
-]
+KEYWORDS = []
 
 PATTERNS = re.compile("|".join(KEYWORDS), flags=re.IGNORECASE)
 
